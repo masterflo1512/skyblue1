99d56180e6d2b3b88b8a07f985583a29f1334d6a
masterflo1512 <sakharovbig@gmail>
2021-08-29 23:09:21 +0300

    show Bohdan my progress

--- test0.js
+++ test0.js
@@ -1,9 +1,13 @@
 let elem = document.getElementById("elem");
 let position = 0;
+let direction = 1;
 
 function move() {
   elem.style.marginLeft = position + "px";
-  position++;
+  if (position === window.innerWidth) direction = -1;
+  position += direction;
+  if (position === window.innerWidth) direction = 1;
+  position += direction;
 }
 
 setInterval(move, 10);
